d297252adf39e06d1dd60a08c1417855a2b0c50d
ArunJIssac <arunjacobissac@hotmail.com>
2019-02-22 18:35:10 -0500

    createtable

---
+++ python/createTables.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+#connect to DB
+con = psycopg2.connect(
+    host = "localhost",
+    database = "testServer",
+    user = "postgres",
+    password = "password",
+    port = "5432")
+
+#cursor
+cur = con.cursor()
+
+createTable_USDJPY = ("CREATE TABLE USDJPY (dateTime datetime, open DOUBLE, close DOUBLE, high DOUBLE, low DOUBLE)")
+
+cur.execute(createTable_USDJPY)
+
+con.close()
