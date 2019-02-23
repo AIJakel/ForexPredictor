746fb3e8cb395b4287572a3c01e54cfdb9831a58
ArunJIssac <arunjacobissac@hotmail.com>
2019-02-23 00:07:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <code>Site under construction</code>.
           </p>
         </header>
       </div>
