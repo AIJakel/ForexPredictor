edb82680e3e28decfcfe5a7497b38a21b2466821
ArunJIssac <arunjacobissac@hotmail.com>
2019-02-22 19:30:13 -0500

    asdsd

--- python/createTables.py
+++ python/createTables.py
@@ -1,12 +1,22 @@
 import psycopg2
 
-#connect to DB
+#connect to DB local
 con = psycopg2.connect(
     host = "localhost",
     database = "postgres",
     user = "postgres",
     password = "password",
     port = "5432")
+
+#connect to DB Heroku
+# con2 = psycopg2.connect(
+#     host = "ec2-54-83-196-179.compute-1.amazonaws.com",
+#     database = "d7h0bl2kg6v3c2",
+#     user = "nqfromejvqdewm",
+#     password = "d4268ae3afeffc9507eb9537d7ce4c53a82e4a0578a1f16b41ea4d489bad4686",
+#     port = "5432",
+#     URI = "postgres://nqfromejvqdewm:d4268ae3afeffc9507eb9537d7ce4c53a82e4a0578a1f16b41ea4d489bad4686@ec2-54-83-196-179.compute-1.amazonaws.com:5432/d7h0bl2kg6v3c2",
+#     CLI = "heroku pg:psql postgresql-horizontal-37622 --app forexpredictor")
 
 #cursor
 cur = con.cursor()
