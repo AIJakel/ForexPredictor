2b2a999140eac48ddcddab68ebc4c90785531084
ArunJIssac <arunjacobissac@hotmail.com>
2019-02-22 19:23:53 -0500

    tables

--- python/createTables.py
+++ python/createTables.py
@@ -10,9 +10,42 @@ con = psycopg2.connect(
 
 #cursor
 cur = con.cursor()
+createTable_USDJPY = ("CREATE TABLE USDJPY (dateTime timestamp without time zone, open numeric NOT NULL, close numeric NOT NULL, high numeric NOT NULL, low numeric NOT NULL, PRIMARY KEY (dateTime))")
+createTable_EURUSD = ("CREATE TABLE EURUSD (dateTime timestamp without time zone, open numeric NOT NULL, close numeric NOT NULL, high numeric NOT NULL, low numeric NOT NULL, PRIMARY KEY (dateTime))")
+createTable_GBPUSD = ("CREATE TABLE GBPUSD (dateTime timestamp without time zone, open numeric NOT NULL, close numeric NOT NULL, high numeric NOT NULL, low numeric NOT NULL, PRIMARY KEY (dateTime))")
+createTable_AUDUSD = ("CREATE TABLE AUDUSD (dateTime timestamp without time zone, open numeric NOT NULL, close numeric NOT NULL, high numeric NOT NULL, low numeric NOT NULL, PRIMARY KEY (dateTime))")
+createTable_USDCAD = ("CREATE TABLE USDCAD (dateTime timestamp without time zone, open numeric NOT NULL, close numeric NOT NULL, high numeric NOT NULL, low numeric NOT NULL, PRIMARY KEY (dateTime))")
+createTable_USDCHF = ("CREATE TABLE USDCHF (dateTime timestamp without time zone, open numeric NOT NULL, close numeric NOT NULL, high numeric NOT NULL, low numeric NOT NULL, PRIMARY KEY (dateTime))")
+createTable_NZDUSD = ("CREATE TABLE NZDUSD (dateTime timestamp without time zone, open numeric NOT NULL, close numeric NOT NULL, high numeric NOT NULL, low numeric NOT NULL, PRIMARY KEY (dateTime))")
 
-createTable_USDJPY = ("CREATE TABLE USDJPY (dateTime TIMESTAMP PRIMARY KEY, open DOUBLE, close DOUBLE, high DOUBLE, low DOUBLE)")
+try:
+    cur.execute(createTable_USDJPY)
+except:
+    ...
+try:    
+    cur.execute(createTable_EURUSD)
+except:
+    ...
+try: 
+    cur.execute(createTable_GBPUSD)
+except:
+    ...
+try: 
+    cur.execute(createTable_AUDUSD)
+except:
+    ...
+try: 
+    cur.execute(createTable_USDCAD)
+except:
+    ...
+try: 
+    cur.execute(createTable_USDCHF)
+except:
+    ...
+try: 
+    cur.execute(createTable_NZDUSD)
+except:
+    ...
 
-cur.execute(createTable_USDJPY)
-
+con.commit()
 con.close()
