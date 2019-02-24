e0ce081970596de3a2d9c25023419b9f38c7c938
ArunJIssac <arunjacobissac@hotmail.com>
2019-02-24 18:14:26 -0500

    get all historical -
    THIS REPLACES< DOES NOT APPEND.

--- python/api.py
+++ python/api.py
@@ -1,9 +1,11 @@
 
 import requests
-import createDBCon
 import fxcmpy
 import pandas
 import createDBCon
+from sqlalchemy import create_engine
+import psycopg2 
+import io
 
 cur = createDBCon.con.cursor()
 #currency layer API key
@@ -24,23 +26,43 @@ con = fxcmpy.fxcmpy(access_token=api_KEY_FX, log_level='error', server='demo')
 data = pandas.DataFrame()
 tradingPairs = {
     "USD/JPY":'usd_jpy',
-    "EUR/USD":'EUR_USD',
-    "GBP/USD":'GBP_USD',
-    "AUD/USD":'AUD_USD',
-    "USD/CAD":'USD_CAD',
-    "USD/CHF":'USD_CHF',
-    "NZD/USD":'NZD_USD'
+    "EUR/USD":'eur_usd',
+    "GBP/USD":'gbp_usd',
+    "AUD/USD":'aud_usd',
+    "USD/CAD":'usd_cad',
+    "USD/CHF":'usd_chf',
+    "NZD/USD":'nzd_usd'
 }
 
 for key, value in tradingPairs.items():
-    data = con.get_candles(key, period='H1', number=5)
+    data = con.get_candles(key, period='H1', number=10)
     #print(data.head())
+    """    host = "localhost",
+    database = "postgres",
+    user = "postgres",
+    password = "password",
+    port = "5432")"""
 
-    for index, row in data.iterrows():
+    engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5432/postgres')
+    data.head(0).to_sql(value, engine, if_exists='replace',index=True) #truncates the table, can also use append: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
+
+    conn = engine.raw_connection()
+    curnew = conn.cursor()
+    output = io.StringIO()
+    data.to_csv(output, sep='\t', header=False, index=True)
+    output.seek(0)
+    contents = output.getvalue()
+    print(value + " _ " + contents)
+    curnew.copy_from(output, value, null="") # null values become ''
+    conn.commit()
+
+    """for index, row in data.iterrows():
         insert = "INSERT INTO {} (date, bidopen, bidclose, bidhigh, bidlow, askopen, askclose, askhigh, asklow) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
         cur.execute(insert.format(value),(index.to_pydatetime(),row['bidopen'],row['bidclose'],row['bidhigh'],row['bidlow'],row['askopen'],row['askclose'],row['askhigh'],row['asklow'],))
         createDBCon.con.commit()
         break
-        
-createDBCon.con.close()
-con.close()
+    """    
+
+conn.close() #engine connect
+createDBCon.con.close() #DB connect
+con.close() #API FOREX
