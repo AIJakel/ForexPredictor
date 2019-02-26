f913c604f6339a34be808381042c3cd42411dd57
ArunJIssac <arunjacobissac@hotmail.com>
2019-02-26 11:01:46 -0500

    change with working api.py

--- python/api.py
+++ python/api.py
@@ -3,31 +3,29 @@ import requests
 import fxcmpy
 import pandas
 from sqlalchemy import create_engine
-import psycopg2 
 import io
 import tokens
 import constants
+import createDBCon
 
-cur = createDBCon.con.cursor()
 
 con = fxcmpy.fxcmpy(access_token=tokens.FXCM_API_KEY, log_level='error', server='demo')
 #print(con.get_instruments())
 
 data = pandas.DataFrame()
-
+db = createDBCon.DATABASES['local']
 for key, value in constants.TRADED_PAIRS.items():
     data = con.get_candles(key, period='D1', number=3650)
 
     engine_string = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(
-        user = db['USER'],
-        password = db['PASSWORD'],
-        host = db['HOST'],
-        port = db['PORT'],
-        database = db['NAME']
+        user =      db['USER'],
+        password =  db['PASSWORD'],
+        host =      db['HOST'],
+        port =      db['PORT'],
+        database =  db['NAME']
     )
 
     engine = create_engine(engine_string)
-    engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5432/postgres')
     data.head(0).to_sql(value, engine, if_exists='replace',index=True) #truncates the table, can also use append: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
 
     conn = engine.raw_connection()
