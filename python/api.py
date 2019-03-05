726208803f8f457858ee1770c7a4cbe90d732f82
ArunJIssac <arunjacobissac@hotmail.com>
2019-03-05 11:03:47 -0500

    Update api.py

--- python/api.py
+++ python/api.py
@@ -1,4 +1,3 @@
-
 import requests
 import fxcmpy
 import pandas
@@ -7,12 +6,12 @@ import io
 import tokens
 import constants
 
-
+print("Connecting to API..")
 con = fxcmpy.fxcmpy(access_token=tokens.FXCM_API_KEY, log_level='error', server='demo')
-
+print("Connection Status: " + con.connection_status)
 data = pandas.DataFrame()
 db = constants.DATABASES['local']
-
+print("Writing data to tables...")
 for key, value in constants.TRADED_PAIRS.items():
     data = con.get_candles(key, period='D1', number=3650)
 
@@ -26,16 +25,15 @@ for key, value in constants.TRADED_PAIRS.items():
 
     engine = create_engine(engine_string)
     data.head(0).to_sql(value, engine, if_exists='replace',index=True) #truncates the table, can also use append: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
-
     conn = engine.raw_connection()
     curnew = conn.cursor()
     output = io.StringIO()
     data.to_csv(output, sep='\t', header=False, index=True)
     output.seek(0)
     contents = output.getvalue()
-    print(value + " _ " + contents)
     curnew.copy_from(output, value, null="") # null values become ''
     conn.commit() 
+    print("Write Complete for: " + key)
 
 conn.close() #engine connect
 con.close() #API FOREX
