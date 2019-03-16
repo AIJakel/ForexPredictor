9ea8d35e169f79cfb021f723044714746a04cd51
Alex Jakel <sircapalex@gmail.com>
2019-03-16 13:33:49 -0400

    imported axios

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <code>Site under construction</code>.
+            <code>Site under construction!</code>.
           </p>
         </header>
       </div>
